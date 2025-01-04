fa435a5f7c1e15488446463b70230b66c3b3d3cc
qr96 <qr96@naver.com>
2025-01-05 00:26:07 +0900

    mod remove method

--- TestProject_Unity6/Assets/Scripts/UI/VirtualButtonLayout.cs
+++ TestProject_Unity6/Assets/Scripts/UI/VirtualButtonLayout.cs
@@ -1,21 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class VirtualButtonLayout : UILayout
 {
-    public KButton item0Button;
-
-    public IconSlot item0Slot;
+    public List<KButton> itemButtons = new List<KButton>();
+    public List<IconSlot> iconSlots = new List<IconSlot>();
 
     private void Start()
     {
-        item0Button.onClick.AddListener(() =>
-        {
-            Managers.GameData.UseItem(0);
-        });
+
     }
 
     public void StartItemCoolTime(int id, float coolTime)
     {
-        item0Slot.StartCoolTime(coolTime);
+        iconSlots[id].StartCoolTime(coolTime);
     }
 }
