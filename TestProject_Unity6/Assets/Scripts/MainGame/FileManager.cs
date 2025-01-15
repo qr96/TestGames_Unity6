cfce9e756f18c51107dbeadcf4678edb29029f07
qr96 <qr96@naver.com>
2025-01-15 23:02:59 +0900

    mod FileManager
    동일한 데이터 저장하지 않도록 방어처리.

--- TestProject_Unity6/Assets/Scripts/MainGame/FileManager.cs
+++ TestProject_Unity6/Assets/Scripts/MainGame/FileManager.cs
@@ -1,19 +1,33 @@
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
 public class FileManager : MonoBehaviour
 {
+    Dictionary<Type, string> lastSaveData = new Dictionary<Type, string>();
+
     public void SaveData<T>(string directoryPath, string fileName, T targetObject)
     {
+        var objectType = targetObject.GetType();
+        var fullPath = $"{Application.persistentDataPath}/{directoryPath}/{fileName}";
+        var objectJson = JsonConvert.SerializeObject(targetObject);
+
+        if (lastSaveData.ContainsKey(objectType))
+        {
+            if (lastSaveData[objectType].Equals(objectJson))
+            {
+                Debug.Log("SaveData() Same data saved already");
+                return;
+            }
+        }
+
+        lastSaveData[objectType] = objectJson;
+
         try
         {
             Directory.CreateDirectory($"{Application.persistentDataPath}/{directoryPath}");
-
-            var fullPath = $"{Application.persistentDataPath}/{directoryPath}/{fileName}";
-            var objectJson = JsonConvert.SerializeObject(targetObject);
-
             File.WriteAllText(fullPath, objectJson);
         }
         catch (Exception ex)
