ffc17ebafb474153c1e4f0797351d74a8e3498dc
qr96 <qr96@naver.com>
2025-01-23 23:12:39 +0900

    mod MapMaker

--- TestProject_Unity6/Assets/Scripts/Tool/MapMaker.cs
+++ TestProject_Unity6/Assets/Scripts/Tool/MapMaker.cs
@@ -1,10 +1,14 @@
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using static TableData;
 
 public class MapMaker : MonoBehaviour
 {
-    readonly string MapInfoDirectoryPath = "MapMaker";
+    readonly string MapInfoDirectoryPath = "Resources/Jsons/MapInfos";
+    readonly string MapUnitDirectoryPath = "Resources/Jsons/MapUnits";
 
     public int mapCode;
     public int unitCode;
@@ -13,6 +17,19 @@ public class MapMaker : MonoBehaviour
 
     public List<MapUnitInfo> unitInfoList = new List<MapUnitInfo>();
     public List<GameObject> unitObjectList = new List<GameObject>();
+    public MapInfo nowMapInfo = new MapInfo();
+
+    public class MapInfo
+    {
+        public List<WarpInfo> startPoints = new List<WarpInfo>();
+        public List<WarpInfo> portals = new List<WarpInfo>();
+    }
+
+    public class WarpInfo
+    {
+        public int mapCode;
+        public float[] position;
+    }
 
     public void Spawn()
     {
@@ -41,13 +58,14 @@ public class MapMaker : MonoBehaviour
             unitInfo.rotation = new float[] { eulerAngles.x, eulerAngles.y, eulerAngles.z };
         }
 
-        Managers.File.SaveData(MapInfoDirectoryPath, $"{mapCode}.json", unitInfoList);
+        SaveData(MapUnitDirectoryPath, $"{mapCode}.json", unitInfoList);
+        
         Debug.Log("SaveData");
     }
 
     public void LoadData()
     {
-        if (Managers.File.TryLoadData<List<MapUnitInfo>>($"{MapInfoDirectoryPath}/{mapCode}.json", out var loaded))
+        if (TryLoadData<List<MapUnitInfo>>($"{MapUnitDirectoryPath}/{mapCode}.json", out var loaded))
         {
             Debug.Log("LoadData");
             unitInfoList = loaded;
@@ -65,6 +83,44 @@ public class MapMaker : MonoBehaviour
                 unitObjectList.Add(instantiated);
             }
         }
+    }
+
+    public void SaveData<T>(string directoryPath, string fileName, T targetObject)
+    {
+        var objectType = targetObject.GetType();
+        var fullPath = $"{Application.dataPath}/{directoryPath}/{fileName}";
+        var objectJson = JsonConvert.SerializeObject(targetObject);
+
+        try
+        {
+            Directory.CreateDirectory($"{Application.dataPath}/{directoryPath}");
+            File.WriteAllText(fullPath, objectJson);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogException(ex);
+        }
+    }
+
+    public bool TryLoadData<T>(string filePath, out T loadObject)
+    {
+        var fullPath = $"{Application.dataPath}/{filePath}";
+        if (File.Exists(fullPath))
+        {
+            try
+            {
+                var fileData = File.ReadAllText(fullPath);
+                loadObject = JsonConvert.DeserializeObject<T>(fileData);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+        }
+
+        loadObject = default;
+        return false;
     }
 }
 
