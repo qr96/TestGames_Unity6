b54a215c8d3f664f15d6fca6f3f4b4605a3f0fe5
qr96 <qr96@naver.com>
2025-01-27 19:20:39 +0900

    mod monster chase player

--- TestProject_Unity6/Assets/Scripts/MainGame/BaseMonster.cs
+++ TestProject_Unity6/Assets/Scripts/MainGame/BaseMonster.cs
@@ -14,19 +14,57 @@ public class BaseMonster : MonoBehaviour
     public Rigidbody rigid;
 
     public float knockBack;
+    public float knockBackTime;
+    public float moveSpeed;
+
+    GameObject targetPlayer;
+    State nowState;
+    DateTime knockBackEnd;
 
     public int Id { get; private set; }
     public int Code { get; private set; }
 
+    private void Update()
+    {
+        if (nowState == State.Damaged)
+        {
+            if (DateTime.Now > knockBackEnd)
+                nowState = State.Idle;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (targetPlayer != null && nowState != State.Damaged)
+        {
+            var direction = targetPlayer.transform.position - transform.position;
+            direction.y = 0;
+
+            var resultVelocity = direction.normalized * moveSpeed;
+            resultVelocity.y = rigid.linearVelocity.y;
+
+            rigid.linearVelocity = resultVelocity;
+            rigid.rotation = Quaternion.LookRotation(direction);
+
+            nowState = State.Move;
+        }
+    }
+
     public void OnAttacked(Vector3 pushed)
     {
+        rigid.linearVelocity = new Vector3(0f, rigid.linearVelocity.y, 0f);
         rigid.rotation = Quaternion.LookRotation(-pushed);
         rigid.AddForce(pushed.normalized * knockBack, ForceMode.Impulse);
+        targetPlayer = Managers.MonsterManager.player.gameObject;
+
+        nowState = State.Damaged;
+        knockBackEnd = DateTime.Now.AddSeconds(knockBackTime);
     }
 
     public void OnDead()
     {
         gameObject.SetActive(false);
+        targetPlayer = null;
     }
 
     public void OnSpawn(int id, int code)
@@ -34,5 +72,6 @@ public class BaseMonster : MonoBehaviour
         Id = id;
         Code = code;
         gameObject.SetActive(true);
+        nowState = State.Idle;
     }
 }
